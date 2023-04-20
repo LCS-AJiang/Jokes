1a74bf7b218da8ef1ded36d35cfea68fa072a3b2
Audrey Jiang <ajiang@lcs.on.ca>
2023-04-20 09:28:41 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Audrey Jiang on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
